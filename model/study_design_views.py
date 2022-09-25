9e72a1ec9382b8670771f6a215e240bddaefba99
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2022-09-25 17:24:34 +0200

    Fix getting link from contract view

--- model/study_design_views.py
+++ model/study_design_views.py
@@ -84,10 +84,11 @@ class StudyDesignViews():
     with db.session() as session:
       query = """MATCH (sd:StudyDesign {uuid: '%s'})-[]->(sc:StudyCell)-[]->(e:StudyEpoch)
           -[]->(v:Encounter)<-[]-(wfi:WorkflowItem)-[]->(a:Activity)-[]->(sda:StudyData)
-          WITH a.activityName as activity, v.encounterName as visit, sda.studyDataDesc as study_data, sda.crfLink as crf_link
-          RETURN DISTINCT activity, visit, study_data, ecrf_link ORDER BY visit, activity, study_data""" % (uuid)
+          WITH a.activityName as activity, v.encounterName as visit, sda.studyDataName as study_data, sda.crfLink as crf_link
+          RETURN DISTINCT activity, visit, study_data, crf_link ORDER BY visit, activity, study_data""" % (uuid)
       result = session.run(query)
       results = []
       for record in result:
+        print(record)
         results.append({ "visit": record["visit"], "activity": record["activity"], "study_data": record["study_data"], "crf_link": record["crf_link"] })
     return results
