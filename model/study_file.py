f266fea24a4021f3266cd8418dc90e6741ff8446
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2023-11-18 14:25:41 +0100

    Refactor, tidy up

--- model/study_file.py
+++ model/study_file.py
@@ -3,14 +3,13 @@ from .neo4j_connection import Neo4jConnection
 from .study_file_nodes_and_edges import StudyFileNodesAndEdges
 from service.github_service import GithubService
 from service.aura_service import AuraService
-
 from uuid import uuid4
 from usdm_excel import USDMExcel
 
 import os
 import yaml
-import time
 import traceback
+import logging
 
 class StudyFile(Node):
   uuid: str = ""
@@ -33,101 +32,72 @@ class StudyFile(Node):
       try:
         os.mkdir(self.dir_path)
       except Exception as e:
-        self.error = f"Failed to create directory {e}"
+        self.error = f"Failed to create directory"
+        self._log(e, f"{traceback.format_exc()}")
         return False
       else:
         try:
           with open(self.full_path, 'wb') as f:
             f.write(contents)
         except Exception as e:
-          self.error = f"Failed to save file content {e}"
+          self.error = f"Failed to save file content"
+          self._log(e, f"{traceback.format_exc()}")
           return False
         else:
           try:
             session.execute_write(self._create, self.filename, self.full_path, self.uuid)
           except Exception as e:
-            self.error = f"Failed to save file details {e}"
+            self.error = f"Failed to save file details"
+            self._log(e, f"{traceback.format_exc()}")
             return False
           else:
             self.set_status("Loaded")
             return True
 
   def set_status(self, status):
+    self.status = status
     db = Neo4jConnection()
     with db.session() as session:
       session.execute_write(self._set_status, self.uuid, status)
 
   def execute(self):
     try:
-      self.set_status("Processing Excel")
 
+      self.set_status("processing excel file")
       excel = USDMExcel(self.full_path)
       nodes_and_edges = excel.to_neo4j_dict()
       filename = os.path.join("uploads", self.uuid, f"{self.uuid}.yaml")
       with open(f"{filename}", 'w') as f:
         f.write(yaml.dump(nodes_and_edges))
 
-      self.set_status("Processing Nodes")
-
-      print(f"EXE: Dump")
+      self.set_status("processing nodes")
       ne = StudyFileNodesAndEdges(self.dir_path, nodes_and_edges)
       ne.dump()
 
-      self.set_status("Uploading to github")
-
-      print(f"EXE: Github")
+      self.set_status("uploading to github")
       github = GithubService()
       file_list = github.upload_dir(self.uuid, self.dir_path, '*.csv')
 
-      print(f"EXE: Wait")
-      check = True
-      loop_count = 0
-      while check and (loop_count <= 12):
-        file_count = github.file_count(self.uuid)
-        print(f"EXE: File count {loop_count} {file_count} v {len(file_list)}")
-        if file_count >= len(file_list):
-          check = False
-          print(f"EXE: File count done")
-        else:
-          time.sleep(5)
-          loop_count += 1
-          print(f"EXE: Sleep {loop_count}")
-
-      self.set_status("Loading database")
+      self.set_status("loading database")
       aura = AuraService()
       aura.load(self.uuid, self.dir_path, file_list)
 
-      self.set_status("Complete")
+      self.set_status("completed")
       return True
+
     except Exception as e:
-      print(f"EXE: Exception {e}")
-      print(f"{traceback.format_exc()}")
-      self.error = f"Failed to convert file {e}"
+      self.error = f"Failed to process and load study. Was {self.status}"
+      self._log(e, f"{traceback.format_exc()}")
       return False
 
-  # def execute(self):
-  #   db = Neo4jConnection()
-  #   with db.session() as session:
-  #     status = session.execute_write(self._set_status, self.uuid, 'running')
-  #     study = ImportExcel(self.full_path)
-  #     identifier = study.identifier()
-  #     if not identifier == None:
-  #       session.execute_write(self._delete_study, study.identifier())
-  #     result = study.save()
-  #     #print("STUDY:", result)
-  #     study = Study.find(result)
-  #     study_designs = study.study_designs()
-  #     study_design = study_designs[0]
-  #     #print("STUDY DESIGN:", study_design)
-  #     result = study_design.data_contract()
-  #     result = study_design.sdtm_domains()
-  #     status = session.execute_write(self._set_status, self.uuid, 'completed')
-  #     return status
+  def _log(self, e, trace):
+    logging.error(self.error)
+    logging.error(f"Exception {e}\n{trace}")
 
   @staticmethod
   def _create(tx, filename, full_path, uuid):
     query = """
-      CREATE (df:StudyFile {filename: $filename, full_path: $full_path, uuid: $uuid, status: 'initialised'})
+      CREATE (df:StudyFile {filename: $filename, full_path: $full_path, uuid: $uuid, status: 'Initialised'})
       RETURN df.uuid as uuid
     """
     results = tx.run(query, 
@@ -139,21 +109,20 @@ class StudyFile(Node):
       return row["uuid"]
     return None
 
-  @staticmethod
-  def _find_by_path(tx, full_path):
-    query = """
-      MATCH (df:StudyFile {full_path: $full_path})
-      RETURN df.uuid as uuid
-    """
-    results = tx.run(query, full_path=full_path)
-    for row in results:
-      return row['uuid']
-    return None
-
-  @staticmethod
-  def _check_extension(filename):
+  def _check_extension(self, filename):
     return True if filename.endswith('.xlsx') else False
-  
+
+  # @staticmethod
+  # def _find_by_path(tx, full_path):
+  #   query = """
+  #     MATCH (df:StudyFile {full_path: $full_path})
+  #     RETURN df.uuid as uuid
+  #   """
+  #   results = tx.run(query, full_path=full_path)
+  #   for row in results:
+  #     return row['uuid']
+  #   return None
+
   @staticmethod
   def _set_status(tx, uuid, status):
     query = """
@@ -165,4 +134,3 @@ class StudyFile(Node):
     for row in results:
       return StudyFile.wrap(row['sf'])
     return None
-
--- service/github_service.py
+++ service/github_service.py
@@ -3,10 +3,14 @@ from utility.service_environment import ServiceEnvironment
 
 import os
 import glob
+import time
 import traceback
 
 class GithubService():
-    
+  
+  class UploadFail(Exception):
+    pass
+
   def __init__(self):
     self.token = ServiceEnvironment().get("GITHUB_TOKEN")
     self.repo_name = ServiceEnvironment().get("GITHUB_REPO")
@@ -15,8 +19,8 @@ class GithubService():
     self.repo = self.g.get_repo(self.repo_name)
  
   def upload_dir(self, uuid, dir, file_type):
+    result = []
     try:
-      result = []
       print(f"GIT: Dir {dir}")
       files = glob.glob(os.path.join(dir, file_type))
       print(f"GIT: Files {files}")
@@ -28,14 +32,29 @@ class GithubService():
         filename = os.path.join(dir, name)
         self.repo.create_file(filename, 'Study service excel import', data, branch=self.branch_name)
         result.append(name)
-      return result
     except Exception as e:
       print(f"GIT: Upload exception {e}")
       print(f"{traceback.format_exc()}")
-
-  def file_count(self, uuid):
+    else:
+      self._all_visible(dir, len(result))
+      return result
+        
+  def _all_visible(self, dir, target_count):
+    check = True
+    loop_count = 0
+    while check and (loop_count <= 12):
+      file_count = self._file_count(dir)
+      if file_count >= target_count:
+        check = False
+      else:
+        time.sleep(5)
+        loop_count += 1
+    if check:
+      raise self.UploadFail
+  
+  def _file_count(self, dir):
     count = 0
-    contents = self.repo.get_contents(f'uploads/{uuid}', ref=self.branch_name)
+    contents = self.repo.get_contents(dir, ref=self.branch_name)
     while contents:
       file_content = contents.pop(0)
       if file_content.type == "dir":
@@ -43,3 +62,5 @@ class GithubService():
       else:
         count += 1
     return count
+  
+
