30bd817eb8e86b3ec5322a91b7856e98b2916bfe
KWLangendorf <kirsten@langendorf.dk>
2024-04-01 17:06:24 +0200

    Update query for listing raw data - study_design_subject_data.py

--- model/study_design_subject_data.py
+++ model/study_design_subject_data.py
@@ -14,20 +14,25 @@ class StudyDesignSubjectData():
       skip_offset_clause = "SKIP %s LIMIT %s" % (offset, size)
     db = Neo4jConnection()
     with db.session() as session:
-      query = """MATCH (sd:StudyDesign {uuid: '%s'})<-[]-(subj:Subject)<-[]-(dp:DataPoint)-[]->(dtp:StudyBCDataTypeProperty)
-        <-[]-(dt:StudyBCDataType)<-[]-(i:StudyBCItem)<-[]-(bc:StudyBCInstance)
-        RETURN COUNT(dp) AS count
+      query = """MATCH (sd:StudyDesign {uuid: '%s'})-[:ORGANIZATIONS_REL]->(resOrg:ResearchOrganization)-[:MANAGES_REL]->(site:StudySite)<-[:ENROLLED_AT_SITE_REL]-(subj:Subject)<-[:FOR_SUBJECT_REL]-(dp:DataPoint)
+      RETURN COUNT(dp) AS count
       """ % (uuid)
       result = session.run(query)
       count = 0
       for record in result:
         count = record['count']
-      query = """MATCH (sd:StudyDesign {uuid: '%s'})<-[]-(subj:Subject)<-[]-(dp:DataPoint)-[]->(dtp:StudyBCDataTypeProperty)
-        <-[]-(dt:StudyBCDataType)<-[]-(i:StudyBCItem)<-[]-(bc:StudyBCInstance)
-        WITH sd, subj, dp, dtp, dt, i, bc 
-        MATCH (subj)-[]->(si:Site)  
-        RETURN subj.identifier as subject, dp.value as value, si.identifier as site, dtp.uri as data_uri, dtp.name as property, dt.name as data_type, i.name as item, bc.name as bc 
-        ORDER BY site, subject, bc, item, data_type, property %s
+      query = """MATCH (sd:StudyDesign {uuid: '%s'})-[:ORGANIZATIONS_REL]->(resOrg:ResearchOrganization)-[:MANAGES_REL]->(site:StudySite)<-[:ENROLLED_AT_SITE_REL]-(subj:Subject)<-[:FOR_SUBJECT_REL]-(dp:DataPoint)-[:FOR_DC_REL]->(dc:DataContract)-[:PROPERTIES_REL]->(bc_prop:BiomedicalConceptProperty)<-[:PROPERTIES_REL]-(bc:BiomedicalConcept),(s)-[:ENROLLED_AT_SITE_REL]->(site:StudySite)
+      OPTIONAL MATCH (bc_prop)-[:CODE_REL]->(:AliasCode)-[:STANDARD_CODE_REL]->(prop:Code)
+      RETURN subj.identifier as subject, 
+      dp.value as value, 
+      site.name as site, 
+      dp.uri as data_uri, 
+      prop.decode as property, 
+      bc_prop.datatype as data_type, 
+      bc_prop.name as item, 
+      bc.name as bc,
+      dc.uri as contract_uri
+      ORDER BY site, subject, bc, item, data_type, property %s
       """ % (uuid, skip_offset_clause)
       print(query)
       result = session.run(query)
@@ -41,7 +46,8 @@ class StudyDesignSubjectData():
           "item": record["item"], 
           "data_type": record["data_type"], 
           "property": record["property"], 
-          "data_uri": record["data_uri"] 
+          "data_uri": record["data_uri"],
+          "contract_uri": record["contract_uri"]
         }
         results.append(final_record)
     result = {'items': results, 'page': page, 'size': size, 'filter': filter, 'count': count }
