de5e5a16c7f63270dcd8e3de9c9e15a346780645
Johannes Ulander <jozza@users.noreply.github.com>
2024-04-05 15:13:55 +0200

    Clean up of debug statements

--- model/domain.py
+++ model/domain.py
@@ -21,105 +21,46 @@ class Domain(BaseNode):
         query = self.dm_query()
       else:
         query = self.findings_query()
-      print(query)
+      # print(query)
       rows = session.run(query)
-      # print("rows",rows)
       for row in rows:
-        print("looping rows",row)
-        print("1",list(row.keys()))
         record = { 
-          # 'uuid': row["uuid"], 
           'variable': row["variable"], 
           'value': row["value"], 
           'DOMAIN': row["DOMAIN"], 
           'STUDYID': row["STUDYID"], 
           'USUBJID': row["USUBJID"],
         }
-        # record = {}
-        # record['uuid'] = row["uuid"]
-        # record['variable'] = row["variable"]
-        # record['value'] = row["value"]
-        # record['DOMAIN'] = row["DOMAIN"]
-        # record['STUDYID'] = row["STUDYID"]
-        # record['USUBJID'] = row["USUBJID"]
         if 'uuid' in row.keys():
           record['uuid'] = row['uuid']
-
         if self.name == "DM":
           record['SUBJID'] = row["SUBJECT"]
           record['SITEID'] = row["SITEID"]
-          # record['COUNTRY'] = row["COUNTRY"] 
-          # record['INVNAM'] = row["INVNAM"]
-          # record['INVID'] = row["INVID"]
+          if 'COUNTRY' in row.keys():
+            record['COUNTRY'] = row["COUNTRY"] 
+          if 'INVNAM' in row.keys():
+            record['INVNAM'] = row["INVNAM"]
+          if 'INVID' in row.keys():
+            record['INVID'] = row["INVID"]
         else:
-          if 'test_code' in row.keys():
-            record['test_code'] = row['test_code']
+          record['test_code'] = row['test_code']
           if 'VISIT' in row.keys():
             record['VISIT'] = row["VISIT"]
           if 'TPT' in row.keys():
             record['TPT'] = row["TPT"]
           if 'EPOCH' in row.keys():
             record['EPOCH'] = row["EPOCH"]
-
-        print(2,"record",list(record.keys()))
         results.append(record)
       #print ("RESULTS:", results)
       if self.name == "DM":
         df = self.construct_dm_dataframe(results)
       else:
         df = self.construct_findings_dataframe(results)
-      # result = df.to_dict('index')
-      result = df.to_dict('records')
+      result = df.to_dict('index')
+      # result = df.to_dict('records')
       return result
 
   def dm_query(self):
-    # TODO Add in enabled true for USING BC relationship
-    #  WITH DISTINCT bc, bcr1, sd, sv, fdt, bdt, sdp, wfi
-    #  OPTIONAL MATCH (sv)-[:BC_RESTRICTION]->(bcr2:BiomedicalConceptRef) WHERE IS NULL bcr2 OR bcr1.uuid = bcr2.uuid
-    # query = """
-    #   MATCH (std:StudyDesign)-[]->(sd:StudyDomainInstance {uuid: '%s'})
-    #   WITH std,sd
-    #   MATCH (std)<-[]-(s:Study)-[:STUDY_IDENTIFIER]->(si:StudyIdentifier)-[:STUDY_IDENTIFIER_SCOPE]->(o:Organisation)-[:ORGANISATION_TYPE]->(c:Code {decode: 'Clinical Study Sponsor'})
-    #   WITH si, std, sd
-    #   MATCH (wfi:WorkflowItem)-[:HAS_STUDY_BC_INSTANCE]->(bc:StudyBCInstance)<-[:BC_REF]-(bcr1:BiomedicalConceptRef)<-[:USING_BC]-(sd)-[:HAS_VARIABLE]->(sv:StudyVariable)
-    #     -[:CLINICAL_RECORDING_REF]->(fdt:ClinicalRecordingRef)<-[:CLINICAL_RECORDING_REF]-(bdt:StudyBCDataTypeProperty)
-    #     <-[:FOR_DATA_POINT]-(sdp:DataPoint)
-    #   WITH DISTINCT si, std, sd, bc, sv, fdt, bdt, sdp, wfi 
-    #   MATCH (wfi)-[:WORKFLOW_ITEM_ENCOUNTER]->(v:Encounter)<-[]-(e:StudyEpoch)
-    #   WITH DISTINCT si, std, sd, bc, sv, fdt, bdt, sdp, wfi, v, e
-    #   MATCH (sdp)-[:FOR_SUBJECT]->(subj:Subject)-[:PARTICIPATES_IN]->(std)-[]->(sd)
-    #   WITH DISTINCT si, std, sd, bc, sv, fdt, bdt, sdp, wfi, v, e, subj
-    #   MATCH (subj)-[:AT_SITE]->(site:Site)<-[:WORKS_AT]-(inv:Investigator)
-    #   WITH DISTINCT si, std, sd, bc, sv, fdt, bdt, sdp, wfi, v, e, subj, site, inv
-    #   MATCH (ct:ValueSet)<-[:HAS_RESPONSE]-()<-[:HAS_STUDY_BC_DATA_TYPE]-(StudyBCItem {name: "Test"})<-[:HAS_STUDY_BC_ITEM]-(bc)-[*]->(bdt)
-    #   RETURN DISTINCT sd.name as domain, sv.name as variable, sdp.value as data, wfi.uuid as uuid, v.encounterName as visit, e.studyEpochName as epoch, 
-    #     subj.identifier as subject, ct.notation as test_code, site.identifier as siteid, 
-    #     inv.name as invnam, inv.identifier as invid, site.country_code as country, si.studyIdentifier as studyid ORDER BY subject
-
-    query = """
-      MATCH (sdp:DataPoint)-[:FOR_DATA_POINT]->(bdtp:StudyBCDataTypeProperty)<-[]-(bdt:StudyBCDataType)<-[]-(bi:StudyBCItem)<-[]-(bc:StudyBCInstance)
-        <-[:BC_REF]-(bcr:BiomedicalConceptRef)<-[:USING_BC]-(sdi:StudyDomainInstance {uuid: '%s'})
-      WITH DISTINCT sdp, bdtp, bdt, bi, bc, bcr, sdi
-      MATCH (sdi)-[:HAS_VARIABLE]->(sv:StudyVariable)-[:CLINICAL_RECORDING_REF]->(crr:ClinicalRecordingRef)<-[:CLINICAL_RECORDING_REF]-(bdtp)
-
-      WITH DISTINCT sdp, bdtp, bdt, bi, bc, bcr, sdi, sv, crr
-      MATCH (sv)-[:BC_RESTRICTION]->(bcr2:BiomedicalConceptRef)-[:BC_REF]->(bc2:StudyBCInstance) WHERE bc.name=bc2.name
-
-      WITH DISTINCT sdp, bdtp, bdt, bi, bc, bcr, sdi, sv, crr
-      MATCH (bc)<-[:HAS_STUDY_BC_INSTANCE]-(wfi:WorkflowItem)-[:WORKFLOW_ITEM_ENCOUNTER]->(v:Encounter)<-[]-(e:StudyEpoch)
-      WITH DISTINCT sdp, bdtp, bdt, bi, bc, bcr, sdi, sv, crr, wfi, v, e
-      MATCH (sdp)-[:FOR_SUBJECT]->(subj:Subject)-[:PARTICIPATES_IN]->(sd:StudyDesign)-[]->(sdi)
-      WITH DISTINCT sdp, bdtp, bdt, bi, bc, bcr, sdi, sv, crr, wfi, v, e, subj, sd
-      MATCH (sd)<-[]-(s:Study)-[:STUDY_IDENTIFIER]->(si:StudyIdentifier)-[:STUDY_IDENTIFIER_SCOPE]->(o:Organisation)-[:ORGANISATION_TYPE]->
-        (c:Code {decode: 'Clinical Study Sponsor'})
-      WITH DISTINCT sdp, bdtp, bdt, bi, bc, bcr, sdi, sv, crr, wfi, v, e, subj, sd, si
-      MATCH (subj)-[:AT_SITE]->(site:Site)<-[:WORKS_AT]-(inv:Investigator)
-      WITH DISTINCT sdp, bdtp, bdt, bi, bc, bcr, sdi, sv, crr, wfi, v, e, subj, sd, si, site, inv
-      MATCH (bc)-[:HAS_STUDY_BC_ITEM]->(StudyBCItem {name: "Test"})-[:HAS_STUDY_BC_DATA_TYPE]->()-[:HAS_RESPONSE]->(ct:ValueSet)
-      RETURN DISTINCT sdi.name as domain, sv.name as variable, sdp.value as data, bc.uuid as uuid, v.encounterName as visit, e.studyEpochName as epoch, 
-        subj.identifier as subject, ct.notation as test_code, site.identifier as siteid, 
-        inv.name as invnam, inv.identifier as invid, site.country_code as country, si.studyIdentifier as studyid ORDER BY subject LIMIT 2000
-    """ % (self.uuid)
     # Query as vertical findings
     query = """
       MATCH (sd:StudyDesign)-[:DOMAIN_REL]->(domain:Domain {uuid:'%s'})
@@ -151,27 +92,6 @@ class Domain(BaseNode):
     return query
 
   def findings_query(self):
-    # query = """
-    #   MATCH (sdp:DataPoint)-[:FOR_DATA_POINT]->(bdtp:StudyBCDataTypeProperty)<-[]-(bdt:StudyBCDataType)<-[]-(bi:StudyBCItem)<-[]-(bc:StudyBCInstance)
-    #     <-[:BC_REF]-(bcr:BiomedicalConceptRef)<-[:USING_BC]-(sdi:StudyDomainInstance {uuid: '%s'})
-    #   WITH DISTINCT sdp, bdtp, bdt, bi, bc, bcr, sdi
-    #   MATCH (sdi)-[:HAS_VARIABLE]->(sv:StudyVariable)-[:CLINICAL_RECORDING_REF]->(crr:ClinicalRecordingRef)<-[:CLINICAL_RECORDING_REF]-(bdtp)
-    #   WITH DISTINCT sdp, bdtp, bdt, bi, bc, bcr, sdi, sv, crr
-    #   MATCH (bc)<-[:HAS_STUDY_BC_INSTANCE]-(wfi:WorkflowItem)-[:WORKFLOW_ITEM_ENCOUNTER]->(v:Encounter)<-[]-(e:StudyEpoch)
-    #   WITH DISTINCT sdp, bdtp, bdt, bi, bc, bcr, sdi, sv, crr, wfi, v, e
-    #   MATCH (sdp)-[:FOR_SUBJECT]->(subj:Subject)-[:PARTICIPATES_IN]->(sd:StudyDesign)-[]->(sdi)
-    #   WITH DISTINCT sdp, bdtp, bdt, bi, bc, bcr, sdi, sv, crr, wfi, v, e, subj, sd
-    #   MATCH (sd)<-[]-(s:Study)-[:STUDY_IDENTIFIER]->(si:StudyIdentifier)-[:STUDY_IDENTIFIER_SCOPE]->(o:Organisation)-[:ORGANISATION_TYPE]->
-    #     (c:Code {decode: 'Clinical Study Sponsor'})
-    #   WITH DISTINCT sdp, bdtp, bdt, bi, bc, bcr, sdi, sv, crr, wfi, v, e, subj, sd, si
-    #   MATCH (subj)-[:AT_SITE]->(site:Site)<-[:WORKS_AT]-(inv:Investigator)
-    #   WITH DISTINCT sdp, bdtp, bdt, bi, bc, bcr, sdi, sv, crr, wfi, v, e, subj, sd, si, site, inv
-    #   MATCH (bc)-[:HAS_STUDY_BC_ITEM]->(StudyBCItem {name: "Test"})-[:HAS_STUDY_BC_DATA_TYPE]->()-[:HAS_RESPONSE]->(ct:ValueSet)
-    #   RETURN DISTINCT sdi.name as domain, sv.name as variable, sdp.value as data, bc.uuid as uuid, v.encounterName as visit, e.studyEpochName as epoch, 
-    #     subj.identifier as subject, ct.notation as test_code, site.identifier as siteid, 
-    #     inv.name as invnam, inv.identifier as invid, site.country_code as country, si.studyIdentifier as studyid ORDER BY subject LIMIT 2000
-    # """ % (self.uuid)
-    # Query as vertical findings
     query = """
       match(domain:Domain{uuid:'%s'})-[:VARIABLE_REL]->(var:Variable)-[:IS_A_REL]->(crm:CRMNode)<-[:IS_A_REL]-(bc_prop:BiomedicalConceptProperty), (domain)-[:USING_BC_REL]->(bc:BiomedicalConcept)-[:PROPERTIES_REL]->(bc_prop)<-[:PROPERTIES_REL]-(dc:DataContract)-[:INSTANCES_REL]->(act_inst_main:ScheduledActivityInstance)<-[:RELATIVE_FROM_SCHEDULED_INSTANCE_REL]-(tim:Timing)
       OPTIONAL MATCH(act_inst_main)-[:ENCOUNTER_REL]->(e:Encounter),(act_inst_main)-[:EPOCH_REL]->(epoch:StudyEpoch)
@@ -197,7 +117,6 @@ class Domain(BaseNode):
       order by DOMAIN, USUBJID, test_code, e_order,ord ,VISIT, TPT
       limit 10
     """ % (self.uuid)
-    print("query",query)
     return query
 
   def construct_dm_dataframe(self, results):
@@ -217,9 +136,12 @@ class Domain(BaseNode):
         final_results[key][column_names.index("USUBJID")] = result["USUBJID"]
         final_results[key][column_names.index("SUBJID")] = result["SUBJID"]
         final_results[key][column_names.index("SITEID")] = result["SITEID"]
-        # final_results[key][column_names.index("INVID")] = result["INVID"]
-        # final_results[key][column_names.index("INVNAM")] = result["INVNAM"]
-        # final_results[key][column_names.index("COUNTRY")] = result["COUNTRY"]
+        if "INVID" in result.keys():
+          final_results[key][column_names.index("INVID")] = result["INVID"]
+        if "INVNAM" in result.keys():
+          final_results[key][column_names.index("INVNAM")] = result["INVNAM"]
+        if "COUNTRY" in result.keys():
+          final_results[key][column_names.index("COUNTRY")] = result["COUNTRY"]
       variable_index = [column_names.index(result["variable"])][0]
       variable_name = result["variable"]
       #print("Index:", variable_index)
@@ -260,15 +182,13 @@ class Domain(BaseNode):
   def construct_findings_dataframe(self, results):
     # topic = self.topic()
     topic = self.name+"TESTCD"
-    print("self",self)
     column_names = self.variable_list()
-    print(3,"column_names",column_names)
     final_results = {}
     for result in results:
-      print(3,"result",list(result.keys()))
-
-      # key = "%s.%s" % (result['SUBJID'], result['uuid'])
-      key = "%s.%s.%s.%s" % (result['USUBJID'],result['test_code'], result['VISIT'], result['TPT'])
+      if 'TPT' in result.keys():
+        key = "%s.%s.%s.%s" % (result['USUBJID'],result['test_code'], result['VISIT'], result['TPT'])
+      else:
+        key = "%s.%s.%s" % (result['USUBJID'],result['test_code'], result['VISIT'])
       if not key in final_results:
         record = [""] * len(column_names)
         record[column_names.index("STUDYID")] = result["STUDYID"]
@@ -279,13 +199,13 @@ class Domain(BaseNode):
         record[column_names.index("VISIT")] = result["VISIT"]
         if 'TPT' in result.keys():
           record[column_names.index(self.name+"TPT")] = result["TPT"]
-        record[column_names.index("EPOCH")] = result["EPOCH"]
+        if 'EPOCH' in result.keys():
+          record[column_names.index("EPOCH")] = result["EPOCH"]
         final_results[key] = record
       else:
         record = final_results[key]
       variable_index = [column_names.index(result["variable"])][0]
       record[variable_index] = result["value"]
-      print(4,"record",record)
     df = pd.DataFrame(columns=column_names)
     for key, result in final_results.items():
       df.loc[len(df.index)] = result
@@ -308,7 +228,7 @@ class Domain(BaseNode):
 
   def hide_variable(self, name):
     # TODO This is a quick fix, needs enable/disable flag on Study Domain 
-    # "--TPT", 
+    # "--TPT" needed for VS
     domain_hide_list = [
       "--GRPID", "--REFID", "--SPID", "--NAM",	"--LOINC", "--ANMETH", "--TMTHSN", "--LOBXFL",
       "--DRVFL", "--TOX", "--TOXGR", "--CLSIG",
