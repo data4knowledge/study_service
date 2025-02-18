d5d7d3122ccc1d04146ad66765fdfda82c700278
jozza <jozza@users.noreply.github.com>
2025-02-17 19:20:40 -0500

    Fix: generic label

--- main.py
+++ main.py
@@ -567,16 +567,15 @@ async def get_study_lab_transfer(uuid: str, page: int=0, size: int=0, filter: st
   else:
     raise HTTPException(status_code=404, detail="The requested study design cannot be found")
 
-# @app.get("/v1/{uuid}/datapoint",
-@app.get("/v1/datapoint_form",
-  summary="Get the BC collection forms for a study design",
+@app.get("/v1/dataContract_context",
+  summary="Get the BC and timing for a data contract",
   description="Provides the forms from BCs for a given study design.",
   response_model=dict)
-async def datapoint_form(datapoint: str, page: int=0, size: int=0, filter: str=""):
-  if datapoint:
-    return StudyDesign.datapoint_form(datapoint, page, size, filter)
+async def data_contract_uri(uri: str, page: int=0, size: int=0, filter: str=""):
+  if uri:
+    return StudyDesign.data_contract_specification(uri, page, size, filter)
   else:
-    raise HTTPException(status_code=404, detail="No datapoint provided with api call")
+    raise HTTPException(status_code=404, detail="No uri provided with api call")
 
 @app.get("/v1/studyDesigns/{uuid}/subjectData", 
   summary="Get the subject data for a study design",
