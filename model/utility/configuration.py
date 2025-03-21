f070bb059f0184641c9fa18325de7bbd0711f223
Johannes Ulander <jozza@users.noreply.github.com>
2025-03-21 16:10:10 +0100

    Update configuration.py

--- model/utility/configuration.py
+++ model/utility/configuration.py
@@ -3,6 +3,8 @@ from d4kms_service import Node, Neo4jConnection
 from model.base_node import BaseNode
 from typing import List, Literal
 from uuid import uuid4
+import pandas as pd
+from d4kms_generic import application_logger
 
 DEFAULT_CONFIGURATION = {
   'study_product_bcs': ["Exposure Unblinded"],
@@ -51,6 +53,38 @@ class ConfigurationNode(BaseNode):
     db.close()
     return result
 
+  @classmethod
+  def add_parent_activities(cls, full_path, study_design_uuid):
+    # @NOTE: Workaround: Get create parent activities as they are not in usdm v3
+    try:
+      xl = pd.read_excel(full_path, sheet_name='mainTimeline')
+      parent_activities = {}
+      FIRST_ACTIVITY_ROW = 8
+      parent = None
+      child = None
+      try:
+        for x in xl.to_dict(orient="records")[FIRST_ACTIVITY_ROW:]:
+          if x['Name'] and isinstance(x['Name'], str) and len(x['Name']) > 2:
+            parent = x['Name'].strip()
+            child = x['Main Timeline'].strip()
+            if parent in parent_activities:
+              parent_activities[parent].append(child)
+            else:
+              parent_activities[parent] = [child]
+          # else:
+          #   print("Ignoring x['Name']", x['Name'], x['Name'].__class__)
+        db = Neo4jConnection()
+        with db.session() as session:
+          result = session.execute_write(cls._add_parent_activities, parent_activities, study_design_uuid)
+        db.close()
+      except Exception as e:
+        application_logger.info(f"Could not parse parent activities parent: {parent} child: {child} {e}")
+        return False
+    except Exception as e:
+      application_logger.info(f"Could not read mainTimeline {e}")
+      return False
+    return True
+
   @staticmethod
   def _create(tx, cls, source_params):
     params = []
@@ -94,6 +128,41 @@ class ConfigurationNode(BaseNode):
     for row in result:
       print("Deleted old configuration node(s)",row['count'])
     return None
+
+
+  @staticmethod
+  def _add_parent_activities(tx, parent_activities, study_design_uuid):
+    for parent, children in parent_activities.items():
+      parent_uuid = str(uuid4())
+      query = """
+          CREATE (p:Activity {name: '%s', label: "Parent activity", description: "Parent activity", uuid: '%s', instanceType: 'Activity'})
+          RETURN p
+      """ % (parent, parent_uuid)
+      # print("query", query)
+      result = tx.run(query)
+      parent_created = False
+      for record in result:
+        parent_created = True
+
+      if parent_created:
+        application_logger.info(f"Parent activity created {parent}")
+        for child in children:
+          query = """
+              MATCH (sd:StudyDesign {uuid:'%s'})-[:ACTIVITIES_REL]->(c:Activity {name: '%s'})
+              MATCH (p:Activity {uuid: '%s'})
+              MERGE (p)-[:CHILD_REL]->(c)
+              RETURN sd, p, c
+          """ % (study_design_uuid, child, parent_uuid)
+          # print(f"child: '{child}'")
+          result = tx.run(query)
+          child_created = False
+          for record in result:
+            child_created = True
+          if not child_created:
+            application_logger.warning(f"Failed to create child activity {child}")
+            print("query", query)
+    return ""
+
 
 
 class Configuration():
